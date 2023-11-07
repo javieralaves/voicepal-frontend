14a554bc7685abd13c1511daf90098e3af999205
Javier Alaves <jayalaves@gmail.com>
2023-11-07 18:14:41 +0100

    control icons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,75 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const HomeScreen = ({ navigation }) => {
+  // Component state
+  const [pressedState, setPressedState] = useState("base");
+
+  // Function to toggle pressed state
+  const togglePressed = () => {
+    setPressedState((prevState) =>
+      prevState === "base" ? "expanded" : "base"
+    );
+  };
+
   return (
     <View style={styles.screen}>
-      <View style={styles.logContainer}>
-        <View style={styles.logImage} />
-        <View style={styles.logText}>
-          <Text style={styles.logTitle}>The hypothesis behind VoicePal</Text>
-          <Text style={styles.posted}>Nov 1 • 19:43</Text>
+      <TouchableOpacity
+        style={styles.logContainer}
+        onPress={togglePressed}
+        activeOpacity={0.7}
+      >
+        <View style={styles.baseContent}>
+          <View style={styles.logImage} />
+          <View style={styles.logText}>
+            <Text style={styles.logTitle}>The hypothesis behind VoicePal</Text>
+            <Text style={styles.posted}>Nov 1 • 19:43</Text>
+          </View>
         </View>
-      </View>
+        {pressedState === "expanded" && (
+          <View style={styles.expandedContent}>
+            {/* Add your additional content here */}
+            <View>
+              <View style={styles.progressBar} />
+            </View>
+            <View style={styles.controls}>
+              <TouchableOpacity style={{ padding: 8 }}>
+                <MaterialCommunityIcons
+                  name="sticker-text-outline"
+                  size={24}
+                  color="#8a8a8e"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={{ padding: 8 }}>
+                <MaterialCommunityIcons
+                  name="rewind-15"
+                  size={24}
+                  color="#8a8a8e"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={{ padding: 8 }}>
+                <MaterialCommunityIcons name="play" size={32} color="black" />
+              </TouchableOpacity>
+              <TouchableOpacity style={{ padding: 8 }}>
+                <MaterialCommunityIcons
+                  name="fast-forward-15"
+                  size={24}
+                  color="#8a8a8e"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={{ padding: 8 }}>
+                <MaterialCommunityIcons
+                  name="dots-horizontal-circle-outline"
+                  size={24}
+                  color="#8a8a8e"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
@@ -24,8 +83,7 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   logContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+    alignItems: "left",
     width: "100%",
     padding: 12,
     backgroundColor: "white",
@@ -39,6 +97,11 @@ const styles = StyleSheet.create({
     elevation: 12,
     shadowOpacity: 1,
   },
+  baseContent: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
   logImage: {
     backgroundColor: "black",
     width: 48,
@@ -46,7 +109,6 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
   logText: {
-    width: "100%",
     marginLeft: 12,
     marginTop: 2,
   },
@@ -63,6 +125,24 @@ const styles = StyleSheet.create({
     color: "#8a8a8e",
     textAlign: "left",
     marginTop: 4,
+  },
+  expandedContent: {
+    marginTop: 16,
+    width: "100%",
+  },
+  progressBar: {
+    width: "100%",
+    height: 3,
+    backgroundColor: "rgba(0, 0, 0, 0.08)",
+    borderRadius: 10,
+  },
+  controls: {
+    flexDirection: "row",
+    width: "100%",
+    marginTop: 12,
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 8,
   },
 });
 
