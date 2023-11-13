df4ac7ddc1b9c428e703ba2a4dc66d33f862d34e
Javier Alaves <jayalaves@gmail.com>
2023-11-13 20:22:00 +0100

    navigation tab

--- src/components/BottomTabNavigator.js
+++ src/components/BottomTabNavigator.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+// Screens
 import HomeScreen from "../screens/HomeScreen";
 import InspirationScreen from "../screens/InspirationScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -9,9 +12,41 @@ const Tab = createBottomTabNavigator();
 export default function BottomTabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Inspiration" component={InspirationScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Inspiration"
+        component={InspirationScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="lightning-bolt"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-circle-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
